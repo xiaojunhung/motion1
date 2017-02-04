69f3c4ff1ec50d93829a3d26c25e2f2d70e5a9c1
Xiao,Jun-Hong <a4278910@gmail.com>
2017-02-04 13:12:43 +0800

    Initial Commit

---
+++ motion/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  motion
+//
+//  Created by David on 2017/2/4.
+//  Copyright © 2017年 DavidAppCoda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
