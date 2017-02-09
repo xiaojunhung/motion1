6e004ae04dbd0bc7d9aa3ee19ff1a699bb5db5c8
Xiao,Jun-Hong <a4278910@gmail.com>
2017-02-09 23:17:42 +0800

    AddAttitude
    AddAttitude
    修改介面

---
+++ motion/Header.h
@@ -0,0 +1,14 @@
+//
+//  Header.h
+//  motion
+//
+//  Created by David on 2017/2/9.
+//  Copyright © 2017年 DavidAppCoda. All rights reserved.
+//
+
+#ifndef Header_h
+#define Header_h
+
+#import <sqlite3.h>
+
+#endif /* Header_h */
--- motion/ViewController.swift
+++ motion/ViewController.swift
@@ -13,29 +13,30 @@ class ViewController: UIViewController {
     @IBOutlet weak var attitudeX: UILabel!
     @IBOutlet weak var attitudeY: UILabel!
     @IBOutlet weak var attitudeZ: UILabel!
-    @IBOutlet weak var phoneStatus: UILabel!
-
+    @IBOutlet weak var StartOrStopButton: UIButton!
+    
+    let delegate = UIApplication.shared.delegate as! AppDelegate
+    
     let mm = CMMotionManager()
     var count = 10
+    var AccX:Double = 0.0
+    var AccY:Double = 0.0
+    var AccZ:Double = 0.0
     var AttX:Double = 0.0
     var AttY:Double = 0.0
     var AttZ:Double = 0.0
+    var myTimer : Timer = Timer()
     
     func updateAttitude() {
         count += 1
         if count % 1 == 0 {
-            if AttX > 1 {
-                phoneStatus.text="直立"
-            }else{
-                if AttX < -1 {
-                    phoneStatus.text="倒立"
-                }else{
-                    phoneStatus.text="平放"
-                }
+            self.attitudeX.text="\(AccX)"
+            self.attitudeY.text="\(AccY)"
+            self.attitudeZ.text="\(AccZ)"
+            
+            if AttX > -1 && AttX < 1 && AttY > -1 && AttY < 1{
+                
             }
-            self.attitudeX.text="\(AttX)"
-            self.attitudeY.text="\(AttY)"
-            self.attitudeZ.text="\(AttZ)"
         }
     }
     
@@ -43,30 +44,36 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         mm.startDeviceMotionUpdates(to: OperationQueue()) { (motion, error) in
             if let motion = motion {
+                let acceleration = motion.userAcceleration
+                self.AccX=acceleration.x
+                self.AccY=acceleration.y
+                self.AccZ=acceleration.z
+                
                 let attitude = motion.attitude
                 self.AttX=attitude.pitch
                 self.AttY=attitude.roll
                 self.AttZ=attitude.yaw
             }
         }
-        // Do any additional setup after loading the view, typically from a nib.
-        if #available(iOS 10.0, *) {
-            Timer.scheduledTimer(timeInterval: 1,
-                                 target: self,
-                                 selector: #selector(self.updateAttitude),
-                                 userInfo: nil,
-                                 repeats: true)
-        } else {
-            // Fallback on earlier versions
+    }
+    @IBAction func StartOrStop(_ sender: Any) {
+        if StartOrStopButton.currentTitle == "Start"{
+            myTimer = Timer.scheduledTimer(timeInterval: 1,
+                                           target: self,
+                                           selector: #selector(self.updateAttitude),
+                                           userInfo: nil,
+                                           repeats: true)
+            StartOrStopButton.setTitle("Stop", for: .normal)
+        }else{
+            myTimer.invalidate()
+            StartOrStopButton.setTitle("Start", for: .normal)
         }
-        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
 
