97a45f84a71dae803c4bd9101ff5970db595b063
Xiao,Jun-Hong <a4278910@gmail.com>
2017-02-09 18:35:26 +0800

    First Commit

--- motion/ViewController.swift
+++ motion/ViewController.swift
@@ -7,12 +7,59 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
+    @IBOutlet weak var attitudeX: UILabel!
+    @IBOutlet weak var attitudeY: UILabel!
+    @IBOutlet weak var attitudeZ: UILabel!
+    @IBOutlet weak var phoneStatus: UILabel!
 
+    let mm = CMMotionManager()
+    var count = 10
+    var AttX:Double = 0.0
+    var AttY:Double = 0.0
+    var AttZ:Double = 0.0
+    
+    func updateAttitude() {
+        count += 1
+        if count % 1 == 0 {
+            if AttX > 1 {
+                phoneStatus.text="直立"
+            }else{
+                if AttX < -1 {
+                    phoneStatus.text="倒立"
+                }else{
+                    phoneStatus.text="平放"
+                }
+            }
+            self.attitudeX.text="\(AttX)"
+            self.attitudeY.text="\(AttY)"
+            self.attitudeZ.text="\(AttZ)"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        mm.startDeviceMotionUpdates(to: OperationQueue()) { (motion, error) in
+            if let motion = motion {
+                let attitude = motion.attitude
+                self.AttX=attitude.pitch
+                self.AttY=attitude.roll
+                self.AttZ=attitude.yaw
+            }
+        }
         // Do any additional setup after loading the view, typically from a nib.
+        if #available(iOS 10.0, *) {
+            Timer.scheduledTimer(timeInterval: 1,
+                                 target: self,
+                                 selector: #selector(self.updateAttitude),
+                                 userInfo: nil,
+                                 repeats: true)
+        } else {
+            // Fallback on earlier versions
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
