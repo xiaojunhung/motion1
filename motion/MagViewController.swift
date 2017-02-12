f923b9e3abc2b8c6d122c15539e059aa9dfc2c88
Xiao,Jun-Hong <a4278910@gmail.com>
2017-02-12 15:35:00 +0800

    新增View
    新增上傳地磁資料

---
+++ motion/MagViewController.swift
@@ -0,0 +1,110 @@
+//
+//  MagViewController.swift
+//  motion
+//
+//  Created by 蕭俊鴻 on 12/02/2017.
+//  Copyright © 2017 DavidAppCoda. All rights reserved.
+//
+
+import UIKit
+import CoreMotion
+
+class MagViewController: UIViewController {
+    
+    @IBOutlet weak var TFXAxis: UITextField!
+    @IBOutlet weak var TFYAxis: UITextField!
+    @IBOutlet weak var LBMag: UILabel!
+    
+    let mm = CMMotionManager()
+    var mytimer = Timer()
+    var x : Double = 0.0
+    var y : Double = 0.0
+    var Magx :Double = 0.0
+    var Magy :Double = 0.0
+    var Magz :Double = 0.0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        mm.startMagnetometerUpdates(to: OperationQueue()) { (data, error) in
+            if let data = data {
+                let tmp = data.magneticField
+                self.Magx = tmp.x
+                self.Magy = tmp.y
+                self.Magz = tmp.z
+            }
+        }
+        mytimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.updateLB), userInfo: nil, repeats: true)
+    }
+    
+    func updateLB(){
+        LBMag.text = "X:\(Magx)\nY:\(Magy)\nZ:\(Magz)"
+    }
+    
+    @IBAction func BTNsubmit(_ sender: Any) {
+        if (TFXAxis.text?.isEmpty)! || (TFYAxis.text?.isEmpty)! {
+            showmsg(msg: "請輸入X與Y軸座標")
+            return
+        }
+        x = Double(TFXAxis.text!)!
+        y = Double(TFYAxis.text!)!
+        var mydic = [String:Any]()
+        mydic["Magx"] = Magx
+        mydic["Magy"] = Magy
+        mydic["Magz"] = Magz
+        mydic["X"] = x
+        mydic["Y"] = y
+        //Post
+//        let urlsrt = ""//在這裡輸入網址
+//        let url = URL(string: urlsrt)
+//        let str = mydic.description.replacingOccurrences(of: "[", with: "{").replacingOccurrences(of: "]", with: "}")
+//        var request = URLRequest(url: url!, cachePolicy: .reloadIgnoringLocalAndRemoteCacheData, timeoutInterval: 30)
+//        request.httpBody = str.data(using: .utf8)
+//        request.httpMethod = "POST"
+//        
+//        let config = URLSessionConfiguration.default
+//        let session = URLSession(configuration: config)
+//        let dataTask = session.dataTask(with: request) { (data, response, error) in
+//        if let data = data {
+//                self.showmsg(msg: "已送出")
+//                let html = String(data:data, encoding: .utf8)
+//                print(html!)
+//            }
+//        }
+//        dataTask.resume()
+    }
+    
+    func showmsg(msg : String) {
+        let alert = UIAlertController(title: "訊息視窗", message: msg, preferredStyle: UIAlertControllerStyle.alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func btnClearTF(_ sender: Any) {
+        TFXAxis.text = ""
+        TFYAxis.text = ""
+    }
+
+    @IBAction func onTFXExit(_ sender: Any) {
+        (sender as AnyObject).resignFirstResponder()
+    }
+    @IBAction func onTFYExit(_ sender: Any) {
+        (sender as AnyObject).resignFirstResponder()
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- motion/ViewController.swift
+++ motion/ViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreMotion
+import CoreLocation
 
 class ViewController: UIViewController {
     @IBOutlet weak var StartOrStopButton: UIButton!
@@ -17,43 +18,112 @@ class ViewController: UIViewController {
     let delegate = UIApplication.shared.delegate as! AppDelegate
     
     let mm = CMMotionManager()
+    let lm = CLLocationManager()
     var count = 10
     var AccX:Double = 0.0
     var AccY:Double = 0.0
     var AccZ:Double = 0.0
-    var AttX:Double = 0.0
-    var AttY:Double = 0.0
-    var AttZ:Double = 0.0
-    var mySpeed : Double = 0.0
+    var MagX:Double = 0.0
+    var MagY:Double = 0.0
+    var MagZ:Double = 0.0
+    var x : Double = 0.0
+    var y : Double = 0.0
+    var z : Double = 0.0
     var myTimer : Timer = Timer()
+    var myIphoneID : String = ""
+    var currentTime : String = ""
+    var mydic = [String:Any]()
     
     func updateAttitude() {
-        status.text="AttPitch:\(AttX)"
-        if self.AttX > 1.4 && self.AttX < 1.6 {
-            self.mySpeed = self.mySpeed+self.AccZ * 9.81
+        status.text="Current Time:\(currentTime)\nid:\(myIphoneID)\nAccX:\(AccX)\nAccY:\(AccY)\nAccZ:\(AccZ)\nMagX:\(MagX)\nMagY:\(MagY)\nMagZ:\(MagZ)\nmyDic:\(mydic.count)\nx:\(x)\ny:\(y)\nz:\(z)"
+        let date = NSDate()
+        let calendar = NSCalendar.current
+        let year = String(calendar.component(.year, from: date as Date))
+        var month = String(calendar.component(.month, from: date as Date))
+        var day = String(calendar.component(.day, from: date as Date))
+        var hour = String(calendar.component(.hour, from: date as Date))
+        var minute = String(calendar.component(.minute, from: date as Date))
+        var second = String(calendar.component(.second, from: date as Date))
+        
+        while month.characters.count < 2 {
+            month = "0"+month
         }
-        let sp = NSString(format:"%.2f",self.mySpeed)
-        MyLabel.text = sp as String
+        while day.characters.count < 2 {
+            day = "0"+day
+        }
+        while hour.characters.count < 2 {
+            hour = "0"+hour
+        }
+        while minute.characters.count < 2 {
+            minute = "0"+minute
+        }
+        while second.characters.count < 2 {
+            second = "0"+second
+        }
+        currentTime = "\(year)-\(month)-\(day) \(hour):\(minute):\(second)"
+        var myitem = [String:Any]()
+        myitem["CurrentTime"] = currentTime
+        myitem["X"] = x
+        myitem["Y"] = y
+        myitem["Z"] = z
+        myitem["MagX"] = MagX
+        myitem["MagY"] = MagY
+        myitem["MagZ"] = MagZ
+        mydic[currentTime] = myitem
+        myitem = [:]
+        
+        
+        if myitem.count == 11{
+            //Post
+//            let urlsrt = ""//在這裡輸入網址
+//            let url = URL(string: urlsrt)
+//            let str = mydic.description.replacingOccurrences(of: "[", with: "{").replacingOccurrences(of: "]", with: "}")
+//            var request = URLRequest(url: url!, cachePolicy: .reloadIgnoringLocalAndRemoteCacheData, timeoutInterval: 30)
+//            request.httpBody = str.data(using: .utf8)
+//            request.httpMethod = "POST"
+//            
+//            let config = URLSessionConfiguration.default
+//            let session = URLSession(configuration: config)
+//            let dataTask = session.dataTask(with: request) { (data, response, error) in
+//                if let data = data {
+//                    let html = String(data:data, encoding: .utf8)
+//                    print(html!)
+//                    mydic = [:]
+//                    mydic["myID"] = myIphoneID
+//                }
+//            }
+//            dataTask.resume()
+        }
     }
-    
+    //載入事件
+    //先開啟DeviceMotion並將感測器的數值放入變數
     override func viewDidLoad() {
         super.viewDidLoad()
         mm.startDeviceMotionUpdates(to: OperationQueue()) { (motion, error) in
             if let motion = motion {
-                let acceleration = motion.userAcceleration
-                self.AccX=acceleration.x
-                self.AccY=acceleration.y
-                self.AccZ=acceleration.z
+                let acc = motion.userAcceleration
+                self.AccX=acc.x
+                self.AccY=acc.y
+                self.AccZ=acc.z
                 
-                let attitude = motion.attitude
-                self.AttX=attitude.pitch
-                self.AttY=attitude.roll
-                self.AttZ=attitude.yaw
-                
-                
+                let rot = motion.attitude.rotationMatrix
+                self.x = (acc.x*rot.m11 + acc.y*rot.m21 + acc.z*rot.m31)*9.81
+                self.y = (acc.x*rot.m12 + acc.y*rot.m22 + acc.z*rot.m32)*9.81
+                self.z = (acc.x*rot.m13 + acc.y*rot.m23 + acc.z*rot.m33)*9.81
             }
         }
+        mm.startMagnetometerUpdates(to: OperationQueue()) { (data, error) in
+            if let data = data {
+                let tmp = data.magneticField
+                self.MagX = tmp.x
+                self.MagY = tmp.y
+                self.MagZ = tmp.z
+            }
+        }
+        myIphoneID = (UIDevice.current.identifierForVendor?.uuidString)!
+        mydic["myID"] = myIphoneID
     }
+    
     @IBAction func StartOrStop(_ sender: Any) {
         if StartOrStopButton.currentTitle == "Start"{
             myTimer = Timer.scheduledTimer(timeInterval: 1,
@@ -67,12 +137,15 @@ class ViewController: UIViewController {
             StartOrStopButton.setTitle("Start", for: .normal)
         }
     }
-    
+    func showmsg(msg:String){
+        let alert = UIAlertController(title: "Message", message: msg, preferredStyle: UIAlertControllerStyle.alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
     @IBAction func ShowPlistOnClick(_ sender: Any) {
         
     }
-    
-    
+        
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
