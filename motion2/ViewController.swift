bdc5de560cfe4669b441f9bc21fd0f4a5d4c9be1
Xiao,Jun-Hong <a4278910@gmail.com>
2017-02-22 22:06:22 +0800

    Initial Commit

---
+++ motion2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  motion2
+//
+//  Created by 蕭俊鴻 on 2017/2/22.
+//  Copyright © 2017年 蕭俊鴻. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
